449e96ed1aa406751f38035b6cfddb8322341168
Landon Cayia <lcayia@uvm.edu>
2020-12-08 15:18:15 -0500

    completed day 7, part 1; different method from before

--- challenge_12-07-20.py
+++ challenge_12-07-20.py
@@ -1,27 +1,34 @@
 with open('input/input_12-07-20.txt', 'r') as f:
-    data = [s.rstrip('.') for s in f.read().split('\n')]
+    data = f.readlines()
 
-bags = [[e.strip().split() for e in b.split(',')] for b in data]
+all_rules = {}
 
-all_rules = []
-
-for b in bags:
-    num_rules = len(b)
-    bag_rule_color = b[0][0] + ' ' + b[0][1]
-    bag_rule_contents = []
-    if b[0][4] != 'no':
-        bag_rule_contents.append({int(b[0][4]): b[0][5] + ' ' + b[0][6]})
-    for i in range(1, num_rules):
-        bag_rule_contents.append({int(b[i][0]): b[i][1] + ' ' + b[i][2]})
-    all_rules.append({bag_rule_color: bag_rule_contents})
+for rule in data:
+    bag, contents = rule.split(' bags contain ')
+    contents = contents.split(',')
+    bag_rules = []
+    for content in contents:
+        content = content.split()
+        color = content[1] + ' ' + content[2]
+        bag_rules.append((content[0], color))
+    all_rules[bag] = bag_rules
 
 count_options = 0
+new_options = -1
+colors_to_check = ['shiny gold']
+found_colors = []
 
-for r in all_rules:
-    for key, value in r.items():
-        for i in value:
-            if list(i.values())[0] == 'shiny gold':
-                count_options += 1
-                break
+while new_options != 0:
+    new_options = 0
+    new_colors_to_check = []
+    for rule in all_rules:
+        for item in all_rules[rule]:
+            if item[1] in colors_to_check and rule not in found_colors:
+                new_options += 1
+                new_colors_to_check.append(rule)
+                found_colors.append(rule)
+    count_options += new_options
+    colors_to_check.clear()
+    colors_to_check = new_colors_to_check
 
 print(count_options)
