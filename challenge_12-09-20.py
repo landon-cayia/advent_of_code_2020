e9bf85915fe642762ea3880b828e69e13d96bf8b
Landon Cayia <lcayia@uvm.edu>
2020-12-10 08:32:27 -0500

    completed day 9, part 2

--- challenge_12-09-20.py
+++ challenge_12-09-20.py
@@ -7,6 +7,15 @@ def part1(nums, low, high):
     return current_val
 
 
+def part2(nums, invalid):
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+            iter_values = [k for k in nums[i:j+1]]
+            iter_sum = sum(iter_values)
+            if iter_sum == invalid:
+                return min(iter_values) + max(iter_values)
+
+
 def check_sums(nums, low, high):
     for i in nums[low:high]:
         for j in [k for k in nums[low:high] if k != i]:
@@ -22,4 +31,7 @@ with open('input/input_12-09-20.txt') as f:
 number_list = [int(num) for num in data]
 low_idx = 0
 high_idx = 25
-print(part1(number_list, low_idx, high_idx))
+invalid_num = part1(number_list, low_idx, high_idx)
+print(invalid_num)
+encrypt_break = part2(number_list, invalid_num)
+print(encrypt_break)
